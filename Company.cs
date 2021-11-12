b557fbc0caeed1d0cd1248ea022e9f5f45f7b23a
ausefumair <91733869+ausefumair@users.noreply.github.com>
2021-11-12 13:05:34 +0530

    Add files via upload

---
+++ Company.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_Reservation.Models
+{
+    public class Company
+    {
+        public int company_id { get; set; }
+        public string company_name { get; set; }
+        public string company_address { get; set; }
+        public string email { get; set; }
+        public string details { get; set; }
+        public int city_id { get; set; }
+    }
+
+    public class CompanyPlan
+    {
+        public int plan_id { get; set; }
+        public int company_id { get; set; }
+        public DateTime is_created { get; set; }
+        public int rooms_min { get; set; }
+        public int rooms_max { get; set; }
+    }
+
+    public class UserAccount
+    {
+        public int user_id { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public int company_id { get; set; }
+        public DateTime is_created { get; set; }
+        public DateTime is_updated { get; set; }
+    }
+
+    public class City
+    {
+        public int city_id { get; set; }
+        public string city_name { get; set; }
+        public string postal_code { get; set; }
+        public int country_id { get; set; }
+    }
+
+    public class Country
+    {
+        public int country_id { get; set; }
+        public string country_name { get; set; }
+    }
+
+    public class Hotel
+    {
+        public int hotel_id { get; set; }
+        public string hotel_name { get; set; }
+        public string description { get; set; }
+        public int company_id { get; set; }
+        public int city_id { get; set; }
+        public string hotel_category { get; set; }
+    }
+
+    public class Room
+    {
+        public int room_id { get; set; }
+        public string room_name { get; set; }
+        public string room_type { get; set; }
+        public int current_price { get; set; }
+    }
+
+    public class Reservation
+    {
+        public int reservation_id { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+        public DateTime is_created { get; set; }
+        public DateTime is_updated { get; set; }
+        public int discount_percent { get; set; }
+        public int total { get; set; }
+        public int guest_id { get; set; }
+        public string guest_name { get; set; }
+    }
+
+    public class ReservationStatus
+    {
+        public int status_id { get; set; }
+        public int reservation_id { get; set; }
+        public bool is_reserved { get; set; }
+    }
+
+    public class Guests
+    {
+        public int guest_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+
+}
---
+++ CompanyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Reservation.Models
+{
+    public class CompanyDbContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>().ToTable("Company");
+        }
+    }
+}
